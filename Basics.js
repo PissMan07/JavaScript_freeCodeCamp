74ac75278bf11e619628b34c1a07ac668b63a694
PissMan07 <ekhangv@gmail.com>
2023-02-21 17:41:57 -0600

    initial

---
+++ Basics.js
@@ -0,0 +1,35 @@
+//This is a comment
+
+/*
+    this is a 
+    multi-line 
+    comment
+*/
+
+//Variables
+var a;
+sus = 1;
+
+var b = "sussy";
+var c = "baka";
+var d = b+c //will equal "sussybaka"
+
+//Let Variables: let variables cannot have duplicate names, best practice to use instead of var
+var e = 1;
+var e = 2;
+
+let f = 1;
+
+// Const Variables: used for constant variables (unchangable variables), best practice to have it be all uppercase
+const G = "hi";
+//G = "Bye" //will cause error
+
+//Special Math
+let h = 20;
+h++; //will result in 21 (adds one)
+h--; //will result in 20 (subs one)
+
+let remainder = 5%3; // will return 2 because when 5 is divided by 3 the will be two left over
+
+//Strings
+const I = "This \"baka\" is kind of a sussy \"baka\"" // will return: "This "baka" is kind of a sussy "baka""
