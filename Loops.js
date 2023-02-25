625b433a47134833e73182a1f39e909a78312d9f
PissMan07 <ekhangv@gmail.com>
2023-02-24 20:46:08 -0600

    Loops

---
+++ Loops.js
@@ -0,0 +1,16 @@
+//While
+let a1 = 5;
+const a2 = [];
+//Desending Order
+while (a1 >= 0){ //will loop untill a1 > 0
+    a2.push(a1)
+    a2--
+}
+
+//For Loop
+let b1 = 5;
+const b2 = [];
+//asending Order
+for (i = 0; i <= 5; i++){ //start at zero;run while i<=5;change i++
+    b2.push(i)
+}
