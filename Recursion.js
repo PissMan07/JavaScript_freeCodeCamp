fed75e3a452014a51754dab342b004f768ff716d
PissMan07 <ekhangv@gmail.com>
2023-02-25 13:04:33 -0600

    reursion

---
+++ Recursion.js
@@ -0,0 +1,14 @@
+//Recursion
+function sum(arr, n) {
+  // Only change code below this line
+  if (n <= 0){
+    return 0;
+  } else {
+    return sum(arr,n-1)+arr[n-1]
+  }
+  // Only change code above this line
+}
+
+console.log(sum([2,3,4],0))
+console.log(sum([2,3,4],1))
+console.log(sum([2,3,4],3))
