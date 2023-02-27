41284f6c0d4f9a9295c773de6bd8f388a27d0287
PissMan07 <ekhangv@gmail.com>
2023-02-27 15:00:28 -0600

    Recursion

--- Recursion.js
+++ Recursion.js
@@ -15,3 +15,46 @@ function sum(arr, n) {
 console.log(sum([2,3,4],0))
 console.log(sum([2,3,4],1))
 console.log(sum([2,3,4],3))
+//looks something like this
+//sum(arr, 0) + sum(arr, 1) + sum(arr, 2) + 0
+
+//Advance Recursion
+console.log('ADVANCE RECURSION COUNTDOWN')
+function countdown(n) {
+  if (n < 1) { 
+    return [];
+  } else {//if n is greater than or equal to one proceede
+    const arr = countdown(n - 1); //recursion
+    arr.unshift(n); //make table
+    return arr;
+  }
+}
+console.log(countdown(5))
+//looks something like this
+//unshift 5, unshift 4, unshift 3, unshift 2, unshift 1, unshift []
+//inserting will work from left to right looking like
+//[].unshift(1).unshift(2).unshift(3).unshift.(2).unshift(1)
+//[5,4,3,2,1]
+
+function countUp(n) {
+  if (n < 1) { 
+    return [];
+  } else {//if n is greater than or equal to one proceede
+    const arr = countUp(n - 1); //recursion
+    arr.push(n); //make table
+    return arr;
+  }
+}
+console.log(countUp(5))
+
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum > endNum){
+    return [];
+  }else{
+    const arr = rangeOfNumbers(startNum+1,endNum);
+    arr.unshift(startNum);
+    return arr;
+  }
+};
+
+console.log(rangeOfNumbers(1,4))
