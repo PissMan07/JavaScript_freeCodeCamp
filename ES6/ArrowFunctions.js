7b3fac0d8d55e17bf67125b8bd4bde8159b7de87
PissMan07 <ekhangv@gmail.com>
2023-02-27 16:43:49 -0600

    Arrow Functions

--- ES6/ArrowFunctions.js
+++ ES6/ArrowFunctions.js
@@ -9,4 +9,5 @@ const ArrFunc = (a,b,c) => 'Hello World'
 const A = (a,b) => a+b
 A(10,10) // will return 20
 
-const B = (...args) => args.reduce((a, b) => a + b, 0) //...args allows for varying number of parameters
+//Varying Parameters
+const B = (...args) => args.reduce((a, b) => a + b, 0) //...args allows for varying number of parameters by putting them all into one array
