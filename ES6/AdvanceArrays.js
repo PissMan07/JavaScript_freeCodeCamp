fd1e63b4273665bf3cc3d9270332f771a4dad9c1
PissMan07 <ekhangv@gmail.com>
2023-02-27 18:13:01 -0600

    Advance Array

--- ES6/AdvanceArrays.js
+++ ES6/AdvanceArrays.js
@@ -7,3 +7,10 @@ const B1 = [1,2,3,4,5]
 const ReducedB1 = B1.reduce((a,b)=> a+b) //A is the total and B is the current value, will return 15
 //loops from total(a) starting at zero and adds all values stating at index 0
 //looks like this 0+1=1+2=3+3=6+4=10+5=15
+
+// ReAssigning Variables
+let a = 8, b = 6;
+[a,b] = [b,a] //will set a = 6 and b = 8
+
+const [e,f,,,g,h, ...arr] = [1,2,3,4,5,6,7,8,9] //will set letter to equal index value
+//e = 1, f = 2, g = 5, h = 6, arr = [7,8,9]
