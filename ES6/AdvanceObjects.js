cb86f13a7d45a6bfa3605ead73ec5c16965c4b27
PissMan07 <ekhangv@gmail.com>
2023-02-28 13:24:43 -0600

    Advance Objects

--- ES6/AdvanceObjects.js
+++ ES6/AdvanceObjects.js
@@ -29,3 +29,13 @@ const C1 = {
 const {yesterday:{low:yl,high:yh}} = C1
 
 //Passing destructuring through functions as parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({max,min}) =>(max + min) / 2.0; //passes on max and min from obj
