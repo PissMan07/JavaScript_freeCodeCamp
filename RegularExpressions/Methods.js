c0e71278a55738b85d2ff743e41df91f1b2dddcb
PissMan07 <ekhangv@gmail.com>
2023-03-04 21:27:05 -0600

    Methods

--- RegularExpressions/Methods.js
+++ RegularExpressions/Methods.js
@@ -37,3 +37,17 @@ bogStr.match(bgRegex); //returns undfined
 let ohStr = "Ohhh no";
 let oh1Regex = /oh{3,6} no/i; // if h repeats at min 3 and max 6 times then return entire statement
 let oh2Regex = /oh{3} no/i; // if h repeats at min 3 return entire statement
+
+//Lookahead: check for item, but will not include in match
+let posRegex = /(?=...)/ // include
+let negRegex = /(?!...)/ // exclude
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/ //First checks if it is 6chars long, and then checks if there are two numbers together with anything before it
+
+//checking for mixed grouping: use parenthseses to be more specific
+let myString1 = "Eleanor Roosevelt";
+let myString2 = "Franklin Roosevelt";
+let myString3 = "Franklin D. Roosevelt";
+let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/;
+//((Franklin|Eleanor) check if first name is Franklin or Eleanor
+//(([A-Z]\.?|[A-Z][a-z]+) )>Roosevelt? ?: makes it optioonal; [A-Z]: allows First initial and \.? allows for '.'; [A-Z][a-z]+ allows for full middle name; space outside () makes sure it is seperate from last
+//Roosevelt Makes checks for Roosevelt and allows it to not be present
