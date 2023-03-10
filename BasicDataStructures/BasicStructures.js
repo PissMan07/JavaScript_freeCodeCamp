1c52a37515028796b318d92b4df5a016668a6a8c
PissMan07 <ekhangv@gmail.com>
2023-03-09 21:02:34 -0600

    Basic Structures

--- BasicDataStructures/BasicStructures.js
+++ BasicDataStructures/BasicStructures.js
@@ -7,6 +7,33 @@ function filteredArray(arr, elem) { //make new array that excludes said element 
       }
     }
     return newArr; //return
+  }  
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18)); //will return [[10,8,3],[14,6,23]]
+
+
+//Check If name is in object; 'Alan','Jeff','Sarah', and 'Ryan'
+function isEveryoneHere(userObj) { //ALL elements in every must match in order for it pass true
+  return ['Alan','Jeff','Sarah','Ryan'].every((name) => //[array].every will loop through every element and apply function
+    userObj.hasOwnProperty(name)
+  );
+  /*&& Method: requires that all elements must meet condition in order to pass true
+
+    return userObj.hasOwnProperty("Alan") &&
+      userObj.hasOwnProperty("Jeff") &&
+      userObj.hasOwnProperty("Sarah") &&
+      userObj.hasOwnProperty("Ryan");
+
+  */
+}
+
+
+// For In
+function countOnline(usersObj) { //will return number of people online
+  let n = 0
+  for (let user in usersObj){ //like x will be every element in userobj starting at index zero.
+    if (usersObj[user]['online'] == true){ //you must call object then person then property
+      n++ //add one to n
+    }
   }
-  
-  console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18)); //will return [[10,8,3],[14,6,23]]
+  return n
+}
